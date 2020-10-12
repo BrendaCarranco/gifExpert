af39931917120c49a456f9da1fc51ac389f3c8c8
BrendaCarranco <breen.78@hotmail.com>
2020-10-12 14:28:48 -0500

    added docs

---
+++ docs/precache-manifest.e2f294edce9c2c286771670d575a4e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e93604a9befddc27504d7062a8877a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a600e952e3c72cf0cbed",
+    "url": "/static/css/main.a5e68d2c.chunk.css"
+  },
+  {
+    "revision": "71522f13fd6174d0aef1",
+    "url": "/static/js/2.c90e9a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c90e9a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a600e952e3c72cf0cbed",
+    "url": "/static/js/main.7705bdc3.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
